5041f335399ca6f388f14808c670aa718a51ca61
Alexsander de Santana <alexsander.santana@aluno.cps.sp.gov.br>
2026-05-14 00:58:46 -0300

    lógica de login simples

--- login.js
+++ login.js
@@ -1,29 +1,75 @@
-// LOGIN
-const loginForm = document.getElementById("loginForm");
-
-if(loginForm){
-
-  loginForm.addEventListener("submit", function(event){
-
-    event.preventDefault();
-
-    alert("Login realizado!");
-
-  });
-
+function login(usuario, senha)
+{
+    var usuarios = sincronizar_usuarios();
+    if (usuarios.filter(v => v.usuario == usuario && v.senha == senha).length)
+    {
+        localStorage.setItem("usuario_logado", usuario);
+        alert('entrou na conta com sucesso');
+        return true;
+    }
+    else
+    {
+        alert('usuário não existe');
+        return false;
+    }
 }
 
-// CADASTRO
-const cadastroForm = document.getElementById("cadastroForm");
+function cadastro(nome, email, usuario, senha)
+{
+    var usuarios = sincronizar_usuarios();
+    if (usuarios.filter(v => v.usuario == usuario).length)
+    {
+        alert('usuário já existe');
+        return false;
+    }
+    else
+    {
+        usuarios.push({nome, email, usuario, senha});
+        sincronizar_usuarios(usuarios);
+        alert('criou a conta com sucesso');
+        return true;
+    }
+}
 
-if(cadastroForm){
+var form_login = document.getElementById("loginForm");
+var form_cadastro = document.getElementById("cadastroForm");
 
-  cadastroForm.addEventListener("submit", function(event){
+form_login?.addEventListener("submit", acao_login);
+form_cadastro?.addEventListener("submit", acao_cadastro);
 
+function acao_login(event)
+{
     event.preventDefault();
+    var usuario = event.target.elements.usuario.value;
+    var senha = event.target.elements.senha.value;
+    if (login(usuario, senha))
+    {
+        location.assign("./home.html");
+    }
+}
 
-    alert("Cadastro realizado!");
+function acao_cadastro(event)
+{
+    event.preventDefault();
+    var nome = event.target.elements.nome.value;
+    var email = event.target.elements.email.value;
+    var usuario = event.target.elements.usuario.value;
+    var senha = event.target.elements.senha.value;
+    if (cadastro(nome, email, usuario, senha))
+    {
+        location.assign("./login.html");
+    }
+}
 
-  });
-
+function sincronizar_usuarios(usuarios)
+{
+    if (usuarios?.length)
+    {
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    }
+    else
+    {
+        usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    }
+    return usuarios || [];
 }
